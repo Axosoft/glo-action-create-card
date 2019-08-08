83b8033d0ddabfe871776f4365bf7a9022ec823c
Dan Suceava <dans@axosoft.com>
2019-08-08 10:24:06 -0700

    add action code

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-# Node 12 Template Action
+# GitHub action to create a card in a Glo Board
 
-To get started, click the `Use this template` button on this repository [which will create a new repository based on this template](https://github.blog/2019-06-06-generate-new-repositories-with-repository-templates/).
+Use this action to create a card on a [Glo Board](https://www.gitkraken.com/glo).
+The action requires a board ID, name and a column as inputs.
 
-For info on how to build your first JavaScript action, see the [toolkit docs folder](https://github.com/actions/toolkit/blob/master/docs/node12-action.md).
+## Requirements
+The action requires an auth token in the form of a PAT that you can create in your GitKraken account.
+See the [Personal Access Tokens](https://support.gitkraken.com/developers/pats/) page on our support site.
+
+This token should be stored in your GitHub repo secrets (in repo Settings -> Secrets).
+
+## Inputs
+The `boardId` input is the ID of the Glo Board to create the card in.
+The `name` input is the name of the card.
+The `column` input is the name of a column that already exists in the Glo Board.
+The `description` input is the description for the card (optional).
+The `assignee` input is the username of a user to assign to the card (optional).
+The `label` input is the name of a lable to add to the card (optional).
+
+## Examples
+Add a step in your workflow file to perform this action:
+```yaml
+    steps:
+    - uses: Axosoft/glo-action-create-card@v1
+      with:
+        authToken: ${{ secrets.GLO-PAT }}
+        boardId: '1234'
+        name: 'New card from action'
+        column: 'New'
+        label: 'Bug'
+      id: glo-create
+```
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,94 @@
 import * as core from '@actions/core';
+import GloSDK from '@axosoft/glo-sdk';
+
+interface ICardToCreate {
+  name: string;
+  description?: {
+    text: string;
+  };
+  labels?: {id: string; name: string}[];
+  assignees?: {id: string}[];
+  column_id?: string;
+  position: number;
+}
 
 async function run() {
+  //required
+  const authToken = core.getInput('authToken');
+  const boardId = core.getInput('boardID');
+  const name = core.getInput('name');
+  const columnName = core.getInput('column');
+  const position = 0;
+
+  //optional
+  const description = core.getInput('description');
+  const username = core.getInput('assignee');
+  const labelName = core.getInput('label');
+
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    if (!boardId || !name || !columnName || !authToken) {
+      return;
+    }
+
+    // find the board { id, labels }
+    const board = await GloSDK(authToken).boards.get(boardId, {
+      fields: ['columns', 'labels', 'members']
+    });
+    if (!board) {
+      core.setFailed(`Board ${boardId} not found`);
+      return;
+    }
+
+    let cardToCreate: ICardToCreate = {
+      name,
+      position
+    };
+
+    //set description
+    if (description) {
+      cardToCreate.description = {
+        text: description
+      };
+    }
+
+    // find label
+    if (board.labels) {
+      const label = board.labels.find(l => l.name === labelName);
+      if (label) {
+        cardToCreate.labels = [
+          {
+            id: label.id as string,
+            name: label.name as string
+          }
+        ];
+      }
+    }
+
+    // find assignee
+    if (board.members) {
+      const member = board.members.find(m => m.username === username);
+      if (member) {
+        cardToCreate.assignees = [
+          {
+            id: member.id as string
+          }
+        ];
+      }
+    }
+
+    // find column
+    if (board.columns) {
+      const column = board.columns.find(c => c.name === columnName);
+      if (column) {
+        cardToCreate.column_id = column.id;
+      } else {
+        core.setFailed(`Column ${columnName} not found`);
+        return;
+      }
+    }
+
+    //create card
+    await GloSDK(authToken).boards.cards.create(boardId, cardToCreate);
   } catch (error) {
     core.setFailed(error.message);
   }
